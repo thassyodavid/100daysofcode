e9653b77e34a080e1f5e14f346812109bb728e3a
thassyodavid <113135589+thassyodavid@users.noreply.github.com>
2024-10-14 08:02:03 -0300

    Day 3
    Tasks:
    1. Write a program that reads user input and print it to the console.
    2. Modify the program to read and print different data type inputs (integers, floating-point numbers, strings)

---
+++ Day3_-_input_and_output.py
@@ -0,0 +1,10 @@
+# 1. Write a program that reads user input and print it to the console.
+# 2. Modify the program to read and print different data type inputs (integers, floating-point numbers, strings)
+
+
+name = input('What is your name? ')
+age = int(input('How old are you? '))
+pi = float(input('Type the value of Pi: '))
+
+
+print(f"Hey, my name is {name}, I'm {age}, and the value of pi is approximately: {pi}")
