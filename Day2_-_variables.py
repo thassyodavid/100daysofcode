6d7e73d532eab46d49f17dfa52312fe1d138f390
thassyodavid <113135589+thassyodavid@users.noreply.github.com>
2024-10-14 07:42:45 -0300

    Day 2
    Tasks:
    1. Create variables for your name, age, and favorite color and print them.

---
+++ Day2_-_variables.py
@@ -0,0 +1,6 @@
+#1. Create variables for your name, age, and favorite color and print them.
+
+name = 'Thassyo'
+age = 28
+fav_color = '#0066ff'
+print(f"Hey, my name is {name}, I'm {age}, and {fav_color} is my favorite color.")
