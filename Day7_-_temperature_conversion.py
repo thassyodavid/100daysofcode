2d38f5b4c3e8bde972396f4569e3e8c6db60bf22
thassyodavid <113135589+thassyodavid@users.noreply.github.com>
2024-10-16 10:22:36 -0300

    Day 7
    1. Write a program to convert temperature from Celsius to Fahrenheit. 2. Write a program to convert temperature from Fahrenheit to Celsius

---
+++ Day7_-_temperature_conversion.py
@@ -0,0 +1,6 @@
+#1. Write a program to convert temperature from Celsius to Fahrenheit. 2. Write a program to convert temperature from Fahrenheit to Celsius
+
+celsius = float(input("Enter the temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C is equal to {fahrenheit:.2f}°F")
+
